af1066c046d82597e4cb1b8f328d473a08c317ec
Alex Pagnotta <ap.alex.pagnotta@gmail.com>
2025-02-12 21:05:47 +0100

    feat: improve homepage layout

--- app/(pages)/(homepage)/[[...contentType]]/page.tsx
+++ app/(pages)/(homepage)/[[...contentType]]/page.tsx
@@ -32,34 +32,29 @@ export default async function Homepage({ params }: HomepageProps) {
   return (
     <>
       <main className="w-full flex flex-col gap-64 lg:flex-row lg:justify-between lg:items-start lg:gap-64-px ">
-        <div className="sticky top-container-vertical">
+        <div className="lg:sticky lg:top-container-vertical">
           <Header selectedContentType={contentType} />
         </div>
 
-        <div className="flex flex-col gap-64 items-center bg-theme-background z-10 relative">
-          <div className="h-[120px] w-full top-container-vertical bg-pink-100 sticky rounded-lg lg:hidden" />
+        <div
+          className={cn(
+            "grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-2 sm:grid-flow-row-dense justify-items-center gap-20-px",
+            "w-full max-w-[36rem] mx-auto sm:max-w-none sm:w-[46rem] md:w-[70rem] lg:w-[46rem]"
+          )}
+        >
+          {content.map((item, index) => {
+            const isLarge = index === 3;
 
-          <div
-            className={cn(
-              "border border-black",
-              "grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-2 sm:grid-flow-row-dense justify-items-center gap-20-px",
-              "w-full max-w-[36rem] sm:max-w-none sm:w-[46rem] md:w-[70rem] lg:w-[46rem]"
-            )}
-          >
-            {content.map((item, index) => {
-              const isLarge = index === 3;
-
-              return (
-                <Link
-                  href={item.slug}
-                  key={item.slug}
-                  className={cn("h-[22rem] w-full bg-green-100 rounded-lg", isLarge && "sm:col-span-2")}
-                >
-                  {item.metadata.title}
-                </Link>
-              );
-            })}
-          </div>
+            return (
+              <Link
+                href={item.slug}
+                key={item.slug}
+                className={cn("h-[22rem] w-full bg-green-100 rounded-lg p-24-px", isLarge && "sm:col-span-2")}
+              >
+                {item.metadata.title}
+              </Link>
+            );
+          })}
         </div>
       </main>
     </>
