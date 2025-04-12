d0a78b80d1272be26f59ecfd35247eb434d569c1
Alex Pagnotta <ap.alex.pagnotta@gmail.com>
2025-04-12 17:05:08 +0200

    feat: move homepage header and wrapper in layout

--- app/(pages)/(homepage)/[[...contentType]]/page.tsx
+++ app/(pages)/(homepage)/[[...contentType]]/page.tsx
@@ -1,7 +1,6 @@
 import { ContentType } from "~/features/content/constants";
 import { getAllContent } from "~/features/content/utils.server";
 import { HomepageGrid } from "~/features/homepage/grid/grid";
-import { HomepageHeader } from "~/features/homepage/header/header";
 
 export function generateStaticParams() {
   return [
@@ -26,10 +25,5 @@ export default async function Homepage({ params }: HomepageProps) {
   const contentType = contentTypeParam?.[0];
   const content = await getAllContent(contentType);
 
-  return (
-    <div className="w-full flex flex-col gap-64 lg:flex-row lg:justify-between lg:items-start lg:gap-64-px ">
-      <HomepageHeader selectedContentType={contentType} className="lg:sticky lg:top-container-vertical" />
-      <HomepageGrid items={content} />
-    </div>
-  );
+  return <HomepageGrid items={content} />;
 }
---
+++ app/(pages)/(homepage)/layout.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { useParams } from "next/navigation";
+
+import { type ContentType } from "~/features/content/constants";
+import { RootJsonLDScript } from "~/features/content/metadata/json-ld";
+import { HomepageHeader } from "~/features/homepage/header/header";
+import { Container } from "~/features/layout/container";
+import { ThemeWrapper } from "~/features/layout/theme-wrapper";
+
+export default function HomepageLayout({ children }: { children: React.ReactNode }) {
+  const params = useParams<{ contentType?: ContentType[] }>();
+
+  const contentType = params.contentType?.[0];
+
+  return (
+    <>
+      <ThemeWrapper>
+        <Container>
+          <div className="w-full flex flex-col gap-64 lg:flex-row lg:justify-between lg:items-start lg:gap-64-px ">
+            <HomepageHeader selectedContentType={contentType} className="lg:sticky lg:top-container-vertical" />
+            {children}
+          </div>
+        </Container>
+      </ThemeWrapper>
+
+      <RootJsonLDScript />
+    </>
+  );
+}
